d1eeb57716e3a86b9339727cf91649574de2d574
Alan.Grolleau <alan.grolleau@gmail.com>
2025-07-23 18:58:29 +0200

    Ajout de la logique de date pour la page Jour 1 :
    Condition pour afficher la page uniquement si la date est inférieure au 08 septembre 2025.
    Correction de l'apostrophe pour éviter les erreurs de lint.
    Mise à jour de la page Planning :
    
    Ajout des liens vers les pages Jour 1, Jour 2, Jour 3 et Jour 4.
    Amélioration des composants Cadeau :
    
    Ajout d'un lien autour de l'image pour rediriger vers la page /cadeau-details.
    Optimisation du LCP avec la propriété priority sur l'image.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-import Image from 'next/image';
+import { Cadeau } from '@/components/Cadeau';
 
 export default function Home() {
   return (
     <main
-      className="h-screen w-screen flex items-center justify-center bg-gray-100 bg-cover bg-center opacity-25"
+      className="h-screen w-screen flex items-end justify-center bg-gray-100 bg-cover bg-center opacity-75"
       style={{ backgroundImage: 'url(/images/accueilBackground.png)' }}
     >
-      {/* Contenu de la page */}
+      <Cadeau />
     </main>
   );
 }
